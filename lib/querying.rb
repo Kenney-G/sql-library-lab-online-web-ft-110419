0927fbe39629366a942c591aadb2252ba046ee6c
Ken Goad <goadke@g.cofc.edu>
2020-01-04 18:58:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -45,7 +45,6 @@ def select_character_names_and_number_of_books_they_are_in
   FROM characters
   INNER JOIN character_books
   ON character_books.character_id = characters.id
-  ON character_books.id = books.id
   GROUP BY COUNT(character_books.character_id
 ;"
 end
